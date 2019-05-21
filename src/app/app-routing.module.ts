cef6c4e22563634168eb02a61cca6fd239c1e8ab
John Tan <johnwarrick.tan@gmail.com>
2019-05-20 20:12:58 -0400

    added node_packages

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,25 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { BooksComponent } from './books/books.component';
+import { RecipesComponent } from './recipes/recipes.component'
+import { HomeComponent } from './home/home.component';
+
+
+const appRoutes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'books', component: BooksComponent},
+  { path: 'recipes', component: RecipesComponent}
+  //{path: '**', component: NotFoundComponent}
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(appRoutes, { useHash: true})],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
+
+export const routingComponents = [
+  HomeComponent,
+  BooksComponent,
+  RecipesComponent
+];
